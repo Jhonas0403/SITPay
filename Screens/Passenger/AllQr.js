680e99a5fb5f15bc7dad540300741cd2a41ee756
JHONATAN JESUS ACUÑA HUISACAYNA <acunahuisa@gmail.com>
2022-11-27 22:21:35 -0500

    adding qr list

--- Screens/Passenger/AllQr.js
+++ Screens/Passenger/AllQr.js
@@ -1,21 +1,69 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
 import Label from "../../components/Label";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import QRCode from "react-qr-code";
 
-const AllQr= () =>{
-return (
-    <View style={styles.container}>
+const AllQr = () => {
+  const [id, setId] = useState("");
+  const [data, setData] = useState([]);
+
+  const getIdUser = async () => {
+    const value = await AsyncStorage.getItem("ID");
+    setId(value);
+  };
+  useEffect(() => {
+    getIdUser();
+  }, []);
+
+  const gettinAllQr = () => {
+    axios
+      .get(`http://192.168.1.12:4000/api/code/${id}`)
+      .then((response) => {
+        setData(response.data.result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    gettinAllQr();
+  }, [id !== ""]);
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
         <Label text={"Los QR relacionados a tu cuenta"} />
-    </View>
-)
-}
+        {data.map((val) => {
+          return (
+            <View style={styles.qrContainer}>
+              <QRCode
+                size={256}
+                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                value={id + "-" + val.denoCode}
+                viewBox={`0 0 256 256`}
+              />
+              <Label text={val.denoCode} />
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
+  );
+};
 
 export default AllQr;
 
 const styles = StyleSheet.create({
-    container: {
-      alignItems: "center",
-      flex: 1,
-      backgroundColor: "#F6E8E2",
-    }
+  container: {
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#F6E8E2",
+  },
+  qrContainer:{
+    alignItems:"center",
+    marginTop: 30,
+  }
 });
